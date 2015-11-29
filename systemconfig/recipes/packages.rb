e4025bf138da7ff0f7d9e6f7478fce6b909ede3e
Nelson Estevam <nelson.estevamjr@gmail.com>
2015-11-29 16:42:15 -0200

    add recipes webserver

--- systemconfig/recipes/packages.rb
+++ systemconfig/recipes/packages.rb
@@ -3,16 +3,16 @@
 	package package
 end
 
-package 'httpd24'
-
-service 'httpd24' do
-  supports restart: true, reload: true
-  action %w(enable start)
-end
-
-execute 'copy-index' do
-    cwd '/var/www/html/'
-    command 'wget http://bionexo-opsworks-intermediate.s3-website-us-east-1.amazonaws.com/hello-world.html -O index2.html'   
-end
+#package 'httpd24'
+#
+#service 'httpd24' do
+#  supports restart: true, reload: true
+#  action %w(enable start)
+#end
+#
+#execute 'copy-index' do
+#    cwd '/var/www/html/'
+#    command 'wget http://bionexo-opsworks-intermediate.s3-website-us-east-1.amazonaws.com/hello-world.html -O index2.html'   
+#end
 
 
---
+++ systemconfig/recipes/webserver.rb
@@ -0,0 +1,22 @@
+package 'httpd24'
+
+service 'httpd24' do
+    supports :status => true
+    action [ :enable, :start ]
+end
+
+file '/var/www/html/index.html' do
+  mode 0644
+  content '<!DOCTYPE html>
+<HTML>
+   <HEAD>
+      <TITLE>
+         A Small Hello 
+      </TITLE>
+   </HEAD>
+<BODY>
+   <H1>Hi</H1>
+   <P>This is very minimal "hello world" HTML document.</P>
+</BODY>
+</HTML>'
+end
