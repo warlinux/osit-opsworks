08e9a36a82d7509eecea9af9922d0d0c5e2987ca
Nelson Estevam <nelson.estevamjr@gmail.com>
2015-11-29 14:04:54 -0200

    Add Apache2 Packages

--- systemconfig/recipes/packages.rb
+++ systemconfig/recipes/packages.rb
@@ -2,3 +2,17 @@
 ['ncdu','strace','htop'].each do |package|
 	package package
 end
+
+package 'httpd24'
+
+service 'httpd24' do
+  supports restart: true, reload: true
+  action %w(enable start)
+end
+
+execute 'copy-index' do
+    cwd '/var/www/html/'
+    command 'wget http://bionexo-opsworks-intermediate.s3-website-us-east-1.amazonaws.com/hello-world.html -O index2.html'   
+end
+
+
